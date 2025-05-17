bf307e695a61f57d81171551cd0a8aa52d48c293
devbydenis <hello.denisrahmadi@gmail.com>
2025-05-17 18:43:07 +0700

    add: banner responsive section in home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
 import Navbar from '../components/Navbar'
 
 function HomePage() {
   return (
     <>
       <Navbar />
-      <div>HomePage</div>
+      <main>
+        {/* BANNER HOME */}
+        <section className='flex flex-col gap-4 justify-center items-center mt-7 md:mx-16 lg:mx-56'>
+          <div className='bg-[#FDECE3] px-2 py-4 rounded-full font-extrabold text-orange '>
+            <p>MOVIE TICKET PURCHASES #1 IN INDONESIA</p>
+          </div>
+          <div className='px-7'>
+            <p className='font-normal text-5xl/14 text-center'>Experience the Magic of Cinema: <span className='text-orange font-bold'>Book Your Tickets Today</span></p>
+          </div>
+          <div>
+            <p className='text-lg/6 text-center font-light'>Sign up and get the ticket with a lot of discount</p>
+          </div>
+        </section>
+
+
+      </main>
     </>
   )
 }
