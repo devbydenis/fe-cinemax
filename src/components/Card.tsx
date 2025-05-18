512ef506713dba919ea7f1f7dc7429992e646d61
devbydenis <hello.denisrahmadi@gmail.com>
2025-05-18 20:25:29 +0700

    feat: card hover effect to navigate

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,7 +11,22 @@ function Card(props: CardPRops) {
   return (
     <>
       {/* looping card disini */}
-      <div className="mb-5 h-fit w-[9.5rem] text-center md:w-[18.5rem]">
+      <div className="group relative mb-5 h-fit w-[9.5rem] text-center md:w-[18.5rem]">
+        <div className="all invisible absolute top-0 right-0 left-0 h-56 md:h-[27.75rem] transition duration-300 group-hover:visible group-hover:bg-black group-hover:opacity-50 rounded-2xl"></div>
+        <div className="invisible absolute top-0 right-0 left-0 z-10 flex h-56 md:h-[27.75rem] flex-col items-center justify-center gap-3 group-hover:visible">
+          <Link
+            to={`/movie`}
+            className="text-white outline-white hover:bg-orange w-36 rounded py-3 text-center outline hover:font-bold hover:outline-none"
+          >
+            Details
+          </Link>
+          <Link
+            to="order"
+            className="text-white outline-white hover:bg-orange w-36 rounded py-3 text-center outline hover:font-bold hover:outline-none"
+          >
+            Buy Tickets
+          </Link>
+        </div>
         <div
           className="poster-background mb-2.5 h-56 w-[9.5rem] rounded-2xl md:h-[27.75rem] md:w-[296px]"
           style={{
@@ -38,8 +53,8 @@ function Card(props: CardPRops) {
           </ul>
         )}
         {category === "coming soon" && (
-          <div className="flex justify-center mt-1">
-            <p className="bg-orange-secondary text-orange rounded px-2 py-1 text-[10px] md:px-4 md:py-2 md:text-base w-fit">
+          <div className="mt-1 flex justify-center">
+            <p className="bg-orange-secondary text-orange w-fit rounded px-2 py-1 text-[10px] md:px-4 md:py-2 md:text-base">
               20 Januari 2025
             </p>
           </div>
