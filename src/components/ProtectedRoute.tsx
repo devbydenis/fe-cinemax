df8d3f4f9d997d1a828a7d392c7db63eeac10cc9
devbydenis <hello.denisrahmadi@gmail.com>
2025-06-20 13:40:15 +0700

    fix: auto navigate login bug when refresh browser even user was logged in

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,11 +1,15 @@
-import { useContext } from "react";
+// import { useContext } from "react";
+// import AuthContext from "../pages/auth/AuthContext";
 import { Navigate } from "react-router-dom";
-import AuthContext from "../pages/auth/AuthContext";
+import { useSelector } from "react-redux";
 
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
-  const { isLoggedinRoute } = useContext(AuthContext);
+  // const { isLoggedinRoute } = useContext(AuthContext);
+  const user = useSelector((state: { user: UserState }) => state.user.user);
+  const { isLogin } = user
+  console.log("ProtectedRoute", isLogin)
 
-  if (isLoggedinRoute) {
+  if (isLogin) {
     // kalo udh login tampilin halaman yang diminta (children)
     return children;
   }
