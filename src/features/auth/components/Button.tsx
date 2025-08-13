5a3e6657c26fef3ec7f581838e925857d1fb65d4
devbydenis <hello.denisrahmadi@gmail.com>
2025-08-13 22:51:29 +0700

    fix(auth): disabled button logic

--- src/features/auth/components/Button.tsx
+++ src/features/auth/components/Button.tsx
@@ -3,8 +3,9 @@ import type { ButtonProps } from "../types/auth.types";
 function Button(props: ButtonProps) {
   return (
     <button
-      className="bg-primary uppercase text-background bg-orange active:text-orange active:border-orange h-12 cursor-pointer rounded-full font-bold active:scale-99 active:border-2 active:bg-transparent active:duration-100 hover:bg-orange/90"
+      className="bg-primary uppercase tracking-wider text-background bg-orange active:text-orange active:border-orange h-12 cursor-pointer rounded-full font-bold active:scale-99 active:border-2 active:bg-transparent active:duration-100 hover:bg-orange/90"
       type={props.type}
+      disabled={props.disabled}
     >
       {props.children}
     </button>
